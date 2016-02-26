9822b77e7d261e0674d49185ca26d00486fb7050
kalmik <sergio.kalmik@gmail.com>
2016-02-26 13:48:41 -0300

    wip characters

---
+++ Assets/Scripts/SmoothCamera.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SmoothCamera : MonoBehaviour {
+
+	// Use this for initialization
+	private Camera cam;
+	void Start () {
+		cam = GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		cam.orthographicSize = ((Screen.height/100f) / 4f)*100;
+	}
+}
