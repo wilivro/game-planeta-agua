d47ab54ed9bbe47396071364cf66f4e1564f5dec
Sergio Filipe <sergio.kalmik@gmail.com>
2016-06-03 15:47:28 -0300

    rio

---
+++ Assets/Resources/Scenes/Rio/Scripts/RioBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RioBehaviour : MonoBehaviour {
+
+	// Use this for initialization
+	public GameObject[] garbage;
+	void Start () {
+		StartCoroutine("Runtime");
+	}
+	
+	IEnumerator Runtime() {
+		Vector3 pos;
+		GameObject g;
+		while(true) {
+			for(int i =0; i < Random.Range(15, 20); i++) {
+
+				g = Instantiate(garbage[Random.Range(0, garbage.Length)]);
+				pos = new Vector3(-50f, g.transform.position.y, Random.Range(-5.0f, 10.0f));
+				g.transform.position = pos;
+
+				yield return new WaitForSeconds(Random.Range(0.4f,0.8f));
+			}
+			
+			yield return new WaitForSeconds(10);
+		}
+	}
+}
---
+++ Assets/Resources/Scenes/Rio/Scripts/RioGarbageBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RioGarbageBehaviour : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = Vector3.Lerp(transform.position, transform.position + Vector3.right, 0.1f);
+		if(transform.position.x > 50) Destroy(gameObject);
+	}
+}
