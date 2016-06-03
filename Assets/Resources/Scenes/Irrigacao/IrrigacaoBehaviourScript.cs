63d80e63fe059c1ae03f24aeb1a8f95cdda91e00
Sergio Filipe <sergio.kalmik@gmail.com>
2016-06-03 10:43:04 -0300

    rio

--- Assets/Resources/Scenes/Irrigacao/IrrigacaoBehaviourScript.cs
+++ Assets/Resources/Scenes/Irrigacao/IrrigacaoBehaviourScript.cs
@@ -4,11 +4,9 @@ using System.Collections;
 public class IrrigacaoBehaviourScript : MonoBehaviour {
 
 	// Use this for initialization
-	public WheelBehaviour[] valve;
-	public Vector3 gain;
+	
 
 	Animator anim;
-	float force;
 
 	void Start () {
 		anim = GetComponent<Animator>();
@@ -18,7 +16,11 @@ public class IrrigacaoBehaviourScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		force = (gain.x) * (valve[0].angle)/360 + (gain.y) * (valve[1].angle)/360 + (gain.z) * (valve[2].angle)/360;
-		anim.SetFloat("velocity", force/2.0f + 0.2f);
+
+		float ratio = (3 - IrrigacaoPaddle.values)/5f + 0.2f;
+
+		Vector3 newSize = new Vector3(ratio, ratio, ratio);
+		transform.localScale = Vector3.Lerp(transform.localScale, newSize, 0.8f);
+		anim.SetFloat("velocity", ratio);
 	}
 }
---
+++ Assets/Resources/Scenes/Irrigacao/IrrigacaoPaddle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class IrrigacaoPaddle : MonoBehaviour {
+
+	// Use this for initialization
+	public static int values;
+	public WheelBehaviour[] valve;
+	public Vector3 gain;
+	public float threshold;
+
+	RectTransform rt;
+
+	int[] positions; 
+
+	void Start () {
+		rt = GetComponent<RectTransform>();
+		positions = new int[4];
+		positions[0] = 13;
+		positions[1] = 118;
+		positions[2] = 228;
+		positions[3] = 333;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		values = Convert.ToInt32(Mathf.Abs(valve[0].angle - gain.x) <= threshold) +
+				Convert.ToInt32(Mathf.Abs(valve[1].angle - gain.y) <= threshold) +
+				Convert.ToInt32(Mathf.Abs(valve[2].angle - gain.z) <= threshold);
+
+		rt.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Top, positions[values], 32);
+	}
+}
